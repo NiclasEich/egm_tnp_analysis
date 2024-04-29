c2efe2d87e7ff2012f068c76fb957664fcb15942
Niclas Eich <niclas2102@gmail.com>
2024-04-29 17:12:49 +0200

    Run script for 2017 tnp

---
+++ run_tnp.sh
@@ -0,0 +1,25 @@
+year=$1
+
+echo "1"
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_01.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --checkBins
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_01.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --createBins
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_01.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --createHists
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_01.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --doFit
+
+echo "2"
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_02.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --checkBins
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_02.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --createBins
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_02.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --createHists
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_02.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --doFit
+
+echo "3"
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_03.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --checkBins
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_03.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --createBins
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_03.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --createHists
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_03.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --doFit
+
+echo "4"
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_04.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --checkBins
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_04.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --createBins
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_04.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --createHists
+python tnpEGM_fitter.py etc/config/settings_ele_UL${year}_04.py  --flag  passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2 --doFit
