527785227178b98489c28477252bf54973e1df85
Niclas Eich <niclas2102@gmail.com>
2024-05-07 12:53:01 +0200

    Added hight pt cut

--- cut_and_count.py
+++ cut_and_count.py
@@ -11,7 +11,7 @@ def main(input_file, zmass=False):
     f = ur.open(input_file)
     tree = f["tnpEleTrig"]["fitter_tree"]
     trigger = tree["passHltEle23Ele12CaloIdLTrackIdLIsoVLLeg2"].array()
-    mask = (tree["tag_Ele_pt"].array() > 25 ) & (np.abs(tree["tag_sc_eta"].array()) < 2.5) & (tree["el_q"].array()*tree["tag_Ele_q"].array() < 0) & (tree["el_pt"].array() > 15)
+    mask = (tree["tag_Ele_pt"].array() > 25 ) & ( tree["tag_Ele_pt"].array() < 500) & (np.abs(tree["tag_sc_eta"].array()) < 2.5) & (tree["el_q"].array()*tree["tag_Ele_q"].array() < 0) & (tree["el_pt"].array() > 15)
     if zmass:
         mask = mask & (np.abs(tree["pair_mass"].array() - 91.1876) <= 15)
     target = trigger[mask]
